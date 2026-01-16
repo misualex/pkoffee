2f6ee9c90c104993f8b0ed3ebdf2d2d81575cb49
koskalns <ko18019@edu.lu.lv>
2026-01-16 16:59:14 +0100

    Add CI action (#8)
    * Add class module
    
    * Day 1 solution: refactored code as a python project managed with pixi
    
    * Add file from pkoffee
    
    * added tests folder
    
    * fix pixi dependencies
    
    * Update pixi.toml
    
    * Add pytest
    
    * test: add csv loading as a dataframe test
    
    * test: add column names test
    
    * fixed path
    
    * Add unit tests
    
    * Add GitHub Actions workflow for testing
    
    * Add GITHUB Aaction
    
    * Add action to eun manually
    
    * Add ruff for linting
    
    * Implement comprehensive test workflow with coverage and docs
    
    * Fix ruff
    
    * Fix unit tests: remove matrix, use correct coverage path
    
    * Add explicit pixi install step
    
    * Move pytest-cov to test feature
    
    ---------
    
    Co-authored-by: Vincent Pollet <vincent.pollet@lapp.in2p3.fr>
    Co-authored-by: Mihai Pop <mihai.pop@maxiv.lu.se>
    Co-authored-by: Mykhailo Levytskyi <m.levytskyi@hm.edu>
    Co-authored-by: misualex <popmiaialexandru@gmail.com>

---
+++ src/pkoffee/tests/test_data.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+
+test_data_path = Path(os.getenv("PKOFFEE_DATA", "analysis"))
+
+def test_initial():
+
+    assert 1 == 1
+    #assert fibonacci(1) == 1
+
+def test_load_csv():
+    from pkoffee.data import load_csv,pd
+
+    # Get the path relative to this test file
+    csv_path = test_data_path / "coffee_productivity.csv"
+    
+    df = load_csv(csv_path)
+    assert isinstance(df, pd.DataFrame)
+
+def test_column_names():
+    from pkoffee.data import load_csv
+    # Get the path relative to this test file
+    csv_path = test_data_path / "coffee_productivity.csv"
+    
+    df = load_csv(csv_path)
+    assert 'cups' in df.columns
+    assert 'productivity' in df.columns
